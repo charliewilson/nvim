9227d98176694e81fc7dbd83e8aa41a3107b8fc5
Charlie Wilson <me@charliewilson.co.uk>
2023-11-24 10:53:46 +0000

    disabled 'q' binding to allow for macro recording

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,9 +7,10 @@ local map = vim.api.nvim_set_keymap
 -- <leader>bq - closes a "tab" i.e. buffer, with vs code behaviour.
 -- swaps to the second-previously edited buffer on close, and opens
 -- a blank buffer when you close the last one.
-map(
-  "n",
-  "q",
-  ":bp<bar>sp<bar>bn<bar>bd<CR>",
-  { noremap = true, silent = true, desc = "Close 'tab' (VS-Code style buffer closing)" }
-)
+
+--map(
+--  "n",
+--  "q",
+--  ":bp<bar>sp<bar>bn<bar>bd<CR>",
+--  { noremap = true, silent = true, desc = "Close 'tab' (VS-Code style buffer closing)" }
+--)
