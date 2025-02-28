5307d5676618d3aee3caee80604952cb49b65439
Charlie Wilson <me@charliewilson.co.uk>
2025-02-28 12:50:30 +0000

    fix bug if journal input cancelled

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,6 +10,7 @@ local map = vim.keymap.set
 
 map("n", "<leader>j", function()
   Snacks.input({}, function(val)
+    if not val then return end
     vim.cmd("! journal '" .. val .. "'")
   end)
 end, { noremap = true, silent = true, desc = "Add line to work journal" }
