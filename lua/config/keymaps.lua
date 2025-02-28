b45397534d0d829f83c71f801c6fafba63c6eaed
Charlie Wilson <me@charliewilson.co.uk>
2025-02-28 12:44:13 +0000

    added <leader>j to add to journal inside nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,15 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 
 -- <leader>bq - closes a "tab" i.e. buffer, with vs code behaviour.
 -- swaps to the second-previously edited buffer on close, and opens
 -- a blank buffer when you close the last one.
 
---map(
---  "n",
---  "q",
---  ":bp<bar>sp<bar>bn<bar>bd<CR>",
---  { noremap = true, silent = true, desc = "Close 'tab' (VS-Code style buffer closing)" }
---)
+map("n", "<leader>j", function()
+  Snacks.input({}, function(val)
+    vim.cmd("! journal '" .. val .. "'")
+  end)
+end, { noremap = true, silent = true, desc = "Add line to work journal" }
+)
